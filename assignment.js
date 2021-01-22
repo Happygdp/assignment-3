7f5fce39b4e3c2a37ccc664e5dda8767a9ef643a
happygdp <happygdp00966@gmail.com>
2021-01-22 09:25:30 -0800

    Assignment 3

--- assignment.js
+++ assignment.js
@@ -1,44 +1,74 @@
 
+
+
 // kilometer to meter
 function kilometerToMeter(kilometer){
+    //1 kilometer = 1000 meter
     var meter = kilometer * 1000;
     return meter;
 }
-var distrence = kilometerToMeter(9);
+var distrence = kilometerToMeter(-1);
 console.log(distrence);
 
 
-// 2nd part budget calculat
-function budgetCalculat(watch, phone, leptop){
-var total = watch + phone + leptop;
-return total;
+// 2nd part budget calculator
+function budgetCalculator(watch, phone, laptop){
+    // cost every single item
+    var watchCost = watch * 50;
+    var phoneCost = phone * 100;
+    var laptopCost = laptop * 500;
+    //total cost of all item.
+    var totalCost = watchCost +  phoneCost + laptopCost;
+    return totalCost;
+
 }
-var watch = 23 * 50;
-var phone = 42 * 100;
-var leptop = 54 * 500;
-var sum = budgetCalculat(watch, phone, leptop);
+
+var sum = budgetCalculator(7, 10, 5);
 console.log(sum);
 
 
 
 
-var day = 15;
-var money = 0;
-if (day <= 10){
-money = day * 100;
-}else if(day <= 20){
-var firstpart = 10 * 100;
-var remaning = day - 10;
-var secondPart = remaning * 80;
-money = firstpart + secondPart;
-}else{
-var firstpart = 10 * 100;
-var secondPart = 10 * 80;
-var remaning = day - 20;
-var thirdPart = remaning * 50;
-money = firstpart + secondPart + thirdPart; 
+// 3nd part hotel cost.
+function hotelCost(day){
+    var money = 0;
+    if (day <= 10){
+        money = day * 100;
+    }else if(day <= 20){
+        //cost for first part
+        var firstpart = 10 * 100;
+        var remaning = day - 10;
+        //cost for second part
+        var secondPart = remaning * 80;
+        money = firstpart + secondPart;
+    
+    }else{
+        //cost for first part
+        var firstpart = 10 * 100;
+        //cost for second part
+        var secondPart = 10 * 80;
+        var remaning = day - 20;
+        //cost for third part
+        var thirdPart = remaning * 50;
+        money = firstpart + secondPart + thirdPart;   
+    }
+    return money;
 }
-console.log (money);
+var totalCost = hotelCost(25);
+console.log(totalCost);
 
-// var magaFriend = ['jaavad', 'tompa', 'sopna', 'sakib'];
-// console.log(magaFriend)
+
+// 4th part mega friend.
+function megaFriend(bigName){
+    var naam = bigName[0];
+    //loop for single name
+    for(var i = 0; i < bigName.length; i++){
+        //to find largest name
+        if(naam.length < bigName[i].length){
+            naam = bigName[i];
+        }
+    }
+     return naam;
+}
+var longestName = megaFriend(['jaavad', 'tompa', 'sopna', 'sakib',"Happy Akhter"]); 
+console.log(longestName);
